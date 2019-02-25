70b76ce733e27d7af2e9125f4b4d79386d424647
Matheus Mota <smota.matheus@gmail.com>
2019-02-24 22:26:31 -0300

    adding routes for jwt token creation

--- src/adonisjs/start/routes.js
+++ src/adonisjs/start/routes.js
@@ -1,28 +1,36 @@
 'use strict'
 
+const Route = use('Route')
+const Env   = use('Env')
+
+Route.get('/', () => {  return 'Hello from the jacinto-casemanager'} )
+
+
 /*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
+    v1 routes
 */
 
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+
+Route.group(() => {
+
+					Route.post('login',        'v1/AuthController.login')
+					Route.post('token/create', 'v1/AuthController.tokenCreate')
+					Route.get( 'token/list'  , 'v1/AuthController.tokenCreate')
+
+	              }).prefix('api/v1/auth').middleware([])
 
 
-Route.post('v3/auth/token/create','v3/AuthController.tokenCreate')
+Route.group(() => {
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
 
-Route.post('login', 'UserController.login').middleware('guest')
+
+	              }).prefix('api/v1').middleware([])
+
+
+// Route.post('login', 'v1/UserController.login')
+
+
+
+
 
 Route.get('users/:id', 'UserController.show').middleware('auth')
