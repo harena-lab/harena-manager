d4d47d265eaed04ca986c0f8dcf779412a86bfb9
faguim <pantoja.ti@gmail.com>
2019-04-02 15:40:40 -0300

    test

--- src/adonisjs/app/Controllers/Http/AuthController.js
+++ src/adonisjs/app/Controllers/Http/AuthController.js
@@ -1,24 +1,66 @@
 'use strict'
 
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+// Route.on('/').render('welcome')
+
+Route.get('/', () => { return 'Hello from the Harena\'s casemanager'} )
+
+Route.group(() => {
+	Route.put('/:id', 'v1/CaseController.update')
+	Route.delete('/id', 'v1/CaseController.destroy')
+	Route.post('', 'v1/CaseController.store')
+	Route.get('', 'v1/CaseController.index') 
+}).prefix('/api/v1/cases/').middleware('auth')
+
+Route.post('/auth/register', 'AuthController.register')
+Route.post('/auth/login', 'AuthController.login')
+
+
+
+
+
+
+'use strict'
+
 const User = use('App/Models/User');
 
 class AuthController {
     async register({ request, auth, response }) {
-        let user = await User.create(request.all())
-        
-        console.log('2')
-        //generate token for user;
-        let token = await auth.generate(user)
+        try {
+            let user = await User.create(request.all())
 
-        Object.assign(user, token)
-        return response.json(user)
+            let token = await auth.generate(user)
+            Object.assign(user, token)
+            response.json(user)
+        } catch(error){
+            console.log(error)
+            if (error.code === 'ER_DUP_ENTRY'){
+                response.json({status : 409, message: 'This username is not available. Choose another one.' })
+            }
+        }
     }
 
     async login({ request, auth, response }) {
         let { email, password } = request.all();
+
         try {
-            if (await auth.attempt(email, password)) {
-                let user = await User.findBy('email', email)
+            if (await auth.attempt(username, password)) {
+                let user = await User.findBy('username', username)
                 let token = await auth.generate(user)
 
                 Object.assign(user, token)
@@ -27,14 +69,32 @@ class AuthController {
         }
         catch (e) {
             console.log(e)
-            return response.json({ message: 'You are not registered!' })
+            return response.json({status : 401, message: 'You are not registered!' })
         }
     }
 
-    // async getCases({ request, response }) {
-    //     let posts = await Case.query().with('user').fetch()
-    //     return response.json(posts)
-    // }
+    /**
+   * Update user details.
+   * PUT or PATCH users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ auth, params, request, response }) {
+    try {
+        await auth.check()
+
+        let u = await User.find(params.id)
+    
+        u.email = request.input('email')
+    
+        await u.save()
+        return response.json(u)
+    } catch (error) {
+        response.send(error.message)
+    }
+  }
 }
 
 module.exports = AuthController
--- src/adonisjs/start/routes.js
+++ src/adonisjs/start/routes.js
@@ -21,13 +21,15 @@ const Route = use('Route')
 Route.get('/', () => { return 'Hello from the Harena\'s casemanager'} )
 
 Route.group(() => {
-	Route.put('/:id', 'v1/CaseController.update')
-	Route.delete('/id', 'v1/CaseController.destroy')
+	Route.put(':id', 'v1/CaseController.update')
+	Route.delete('id', 'v1/CaseController.destroy')
 	Route.post('', 'v1/CaseController.store')
 	Route.get('', 'v1/CaseController.index') 
 }).prefix('/api/v1/cases/').middleware('auth')
 
-Route.post('/auth/register', 'AuthController.register')
-Route.post('/auth/login', 'AuthController.login')
-
-
+Route.group(() => {
+	Route.post('register', 'AuthController.register')
+	Route.post('login', 'AuthController.login') 
+	Route.get('logout', 'AuthController.logout') 
+	Route.put('users/:id', 'AuthController.update')
+}).prefix('/auth')
