f7d0d0c7644846370141db195e7f09ac6653b491
Matheus Mota <smota.matheus@gmail.com>
2019-04-03 20:58:13 -0300

    routes definitions

--- src/adonisjs/start/routes.js
+++ src/adonisjs/start/routes.js
@@ -1,36 +1,92 @@
 'use strict'
-
-const Route = use('Route')
-const Env   = use('Env')
-
-Route.get('/', () => {  return 'Hello from the jacinto-casemanager'} )
-
-
+ 
 /*
-    v1 routes
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+| 
+| Architecture Documentation:     https://docs.google.com/presentation/d/1-8s0X-VuXF4b_vi8tbzYNCHU6UwYOUPa7KppUpevaNA/edit#slide=id.g4cba6f4dfb_0_108
+| case-notebook previous mapping: https://github.com/datasci4health/case-notebook/blob/master/notebook/server/notebook-server-rest.ipynb
 */
 
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+// Route.on('/').render('welcome')
+
+Route.get('/', () => { return 'Hello from the Harena\'s casemanager'} )
+
+/* 
+	User CRUD
+*/
+Route.group(() => { 
+
+					Route.post(  '',               'v1/UserController.store')
+					Route.put(   ':id',            'v1/UserController.update')
+					Route.delete(':id',            'v1/UserController.destroy')
+					Route.get(   ':id',            'v1/UserController.index') 
+					Route.get(   ':id/cases',      'v1/UserController.listCases') 
+					Route.get(   ':id/executions', 'v1/UserController.listExecutions') 
+					Route.post(  ':id/executions', 'v1/UserController.newExecution') // input: case_id -> returns execution:{uuid}
+
+}).prefix('/api/v1/user/').middleware('auth')
+
+
+/* 
+	Case CRUD
+*/
+Route.group(() => {
+
+					Route.post(  '',               'v1/CaseController.store')
+					Route.put(   ':id',            'v1/CaseController.update')
+					Route.delete(':id',            'v1/CaseController.destroy')
+					Route.get(   ':id',            'v1/CaseController.index') 
+					Route.get(   '',               'v1/CaseController.index') 	
+					Route.post(  ':id/executions', 'v1/UserController.newExecution')   // input: user_id -> returns execution:{uuid}
+					Route.get(   ':id/executions', 'v1/UserController.listExecutions') // input: user_id -> returns execution:{uuid}
+}).prefix('/api/v1/cases/').middleware('auth')
+
+
+/* 
+	Execution CRUD
+*/
 
 Route.group(() => {
 
-					Route.post('login',        'v1/AuthController.login')
-					Route.post('token/create', 'v1/AuthController.tokenCreate')
-					Route.get( 'token/list'  , 'v1/AuthController.tokenCreate')
+					Route.get(   '',       'v1/ExecutionController.index') // {filter:{ user_id: 14, case_id: 89}}
+					Route.get(   ':id',    'v1/ExecutionController.index') 
+					Route.put(   ':id',    'v1/ExecutionController.update')
+					Route.delete(':id',    'v1/ExecutionController.destroy')
 
-	              }).prefix('api/v1/auth').middleware([])
+}).prefix('/api/v1/execution/').middleware('auth')
 
+
+/* 
+	Template CRUD 
+*/
 
 Route.group(() => {
 
+					Route.get(   '',       'v1/TemplateController.index') 
+
+}).prefix('/api/v1/template/').middleware('auth')
 
 
-	              }).prefix('api/v1').middleware([])
+/* 
+	Auth  
+*/
 
+Route.group(() => {
 
-// Route.post('login', 'v1/UserController.login')
+					// Route.post('register',  'AuthController.register')
+					Route.post('login',     'AuthController.login') 
+					Route.get( 'logout',    'AuthController.logout') 
+					Route.put( 'users/:id', 'AuthController.update')
 
-
-
-
-
-Route.get('users/:id', 'UserController.show').middleware('auth')
+}).prefix('/api/v1/auth/')
