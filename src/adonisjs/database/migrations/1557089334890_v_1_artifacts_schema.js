eb05b4b20060ebfc6d627b2819f4a247a5f69378
Matheus Mota <smota.matheus@gmail.com>
2019-05-05 19:31:08 -0300

    artifact database migration

---
+++ src/adonisjs/database/migrations/1557089334890_v_1_artifacts_schema.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ArtifactsSchema extends Schema {
+
+  up () {
+
+    this.create('artifacts', (table) => {
+      table.increments()
+      table.integer('user_id').unsigned().references('id').inTable('users');
+      table.integer('case_id').unsigned().references('id').inTable('cases');
+      table.string('fs_path',         300).notNullable()
+      table.string('relative_path',   300).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+  	
+    this.drop('artifacts')
+  }
+}
+
+module.exports = ArtifactsSchema
