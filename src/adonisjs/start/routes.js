c30bd6624a67e39d8dff28ab3158aa2a7c5c9642
Matheus Mota <smota.matheus@gmail.com>
2019-05-05 19:29:06 -0300

    major updates in routes - organization and /artifacts addition

--- src/adonisjs/start/routes.js
+++ src/adonisjs/start/routes.js
@@ -1,103 +1,128 @@
 'use strict'
  
+
+const Route   = use('Route')
+
 /*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-| 
-| Architecture Documentation:     https://docs.google.com/presentation/d/1-8s0X-VuXF4b_vi8tbzYNCHU6UwYOUPa7KppUpevaNA/edit#slide=id.g4cba6f4dfb_0_108
-| case-notebook previous mapping: https://github.com/datasci4health/case-notebook/blob/master/notebook/server/notebook-server-rest.ipynb
+|----------------------------------------------------------------------------------------------
+|  index                                                   
+|----------------------------------------------------------------------------------------------
 */
+Route.get('/', () => { return 'Hello from the harena-manager'} )
 
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
 
-// Route.get('/', () => { return 'Hello from the Harena\'s casemanager'} )
 
-const knot = { knot: ' asiudfhiuaqhsdf asiudf nasiudfa sdf' }
-
-/* Matheus created these methods in order to show us how we would supossed to use views */
-// Route.get('/classic', ({ view }) => {
-// 							  return view.render('themes/classic/dt', knot)
-// })
-
-// Route.get('/jacinto', ({ view }) => {
-// 							  return view.render('themes/jacinto/dt', knot)
-// })
-
-/* Test route */
-Route.post('/api/v1/imagetest', 	'TestController.test') 
-
-/* User CRUD */
-Route.post('/api/v1/user/register', 'v1/UserController.store')
-Route.post('/api/v1/user/login', 	'AuthController.login') 
+/*
+|----------------------------------------------------------------------------------------------
+|       api: v1                                                   
+|  resource: /user
+|----------------------------------------------------------------------------------------------
+*/
+Route.post('/api/v1/user/register',                 'v1/UserController.store')
+Route.post('/api/v1/user/login',                    'AuthController.login') 
 Route.group(() => { 
-					Route.get(   '',			   	'v1/UserController.index') 
-					Route.get(   ':id',			   	'v1/UserController.show') 
-					Route.put(   ':id',            	'v1/UserController.update')
-					Route.delete(':id',            	'v1/UserController.destroy')
 
-					Route.get(   ':id/execution', 	'v1/UserController.listExecutions') 
-					Route.post(  'execution', 		'v1/UserController.newExecution') // input: case_id -> returns execution:{uuid}
+                    Route.get(   '',                'v1/UserController.index') 
+                    Route.get(   ':id',             'v1/UserController.show') 
+                    Route.put(   ':id',             'v1/UserController.update')
+                    Route.delete(':id',             'v1/UserController.destroy')
+                    Route.get(   ':id/execution',   'v1/UserController.listExecutions') 
+                    Route.post(  'execution',       'v1/UserController.newExecution') 
+
 }).prefix('/api/v1/user').middleware('auth')
 
-/* 
-	Case CRUD
+
+
+/*
+|----------------------------------------------------------------------------------------------
+|       api: v1                                                   
+|  resource: /case
+|----------------------------------------------------------------------------------------------
 */
 Route.group(() => {
-			Route.post( 'list',            		'v1/CaseController.index') 	
-			Route.get(	':id',   				'v1/CaseController.show')
- 
-			Route.post(  '',               		'v1/CaseController.store')
-			Route.put(   ':id',            		'v1/CaseController.update')
-			Route.delete(':id',            		'v1/CaseController.destroy')
 
-			Route.post(  'new',            		'v1/CaseController.newCase')
+                    Route.post( 'list',                 'v1/CaseController.index')  
+                    Route.get(  ':id',                  'v1/CaseController.show') 
+                    Route.post(  '',                    'v1/CaseController.store')
+                    Route.put(   ':id',                 'v1/CaseController.update')
+                    Route.delete(':id',                 'v1/CaseController.destroy')
+                    Route.post(  'new',                 'v1/CaseController.newCase')
+                    Route.post(  ':id/executions',      'v1/UserController.newExecution')   
+                    Route.get(   ':id/executions',      'v1/UserController.listExecutions') 
 
-			Route.post(  ':id/executions', 		'v1/UserController.newExecution')   // input: user_id -> returns execution:{uuid}
-			Route.get(   ':id/executions', 		'v1/UserController.listExecutions') // input: user_id -> returns execution:{uuid}
 }).prefix('/api/v1/case').middleware('auth')
 
-/* 
-	Execution CRUD
+
+
+/*
+|----------------------------------------------------------------------------------------------
+|       api: v1                                                   
+|  resource: /execution
+|----------------------------------------------------------------------------------------------
 */
 Route.group(() => {
-					Route.get(   '',       'v1/ExecutionController.index') // {filter:{ user_id: 14, case_id: 89}}
-					Route.get(   ':id',    'v1/ExecutionController.index') 
-					Route.put(   ':id',    'v1/ExecutionController.update')
-					Route.delete(':id',    'v1/ExecutionController.destroy')
+
+                    Route.get(   '',                    'v1/ExecutionController.index') 
+                    Route.get(   ':id',                 'v1/ExecutionController.index') 
+                    Route.put(   ':id',                 'v1/ExecutionController.update')
+                    Route.delete(':id',                 'v1/ExecutionController.destroy')
+
 }).prefix('/api/v1/execution/').middleware('auth')
 
-/* 
-	Template CRUD 
+
+
+/*
+|----------------------------------------------------------------------------------------------
+|       api: v1                                                   
+|  resource: /template
+|----------------------------------------------------------------------------------------------
 */
 Route.group(() => {
-					Route.get(   '',       'v1/TemplateController.index') 
+
+                    Route.get(   '',                    'v1/TemplateController.index') 
+
 }).prefix('/api/v1/template/').middleware('auth')
 
-/* 
-	Models CRUD
+
+
+/*
+|----------------------------------------------------------------------------------------------
+|       api: v1                                                   
+|  resource: /models
+|----------------------------------------------------------------------------------------------
 */
 Route.group(() => {
-					Route.get(   '',    'v1/ModelController.index') 
+
+                    Route.get(   '',                    'v1/ModelController.index') 
+
 }).prefix('/api/v1/models/').middleware('auth')
 
-/* 
-	KnotCapsule CRUD
+
+
+/*
+|----------------------------------------------------------------------------------------------
+|       api: v1                                                   
+|  resource: /knot
+|----------------------------------------------------------------------------------------------
 */
 Route.group(() => {
-				Route.get(   '',		'v1/KnotCapsuleController.index') 
-				Route.post(  '',      	'v1/KnotCapsuleController.store')
+
+                    Route.get(   '',                    'v1/KnotCapsuleController.index') 
+                    Route.post(  '',                    'v1/KnotCapsuleController.store')
+
 }).prefix('/api/v1/knot/').middleware('auth')
 
-/* 
-	Auth  
+
+
+/*
+|----------------------------------------------------------------------------------------------
+|       api: v1                                                   
+|  resource: /artifacts
+|----------------------------------------------------------------------------------------------
 */
 Route.group(() => {
-}).prefix('/api/v1/auth')
+
+                    Route.post(  '',                    'v1/ArtifactController.store')
+
+}).prefix('/api/v1/artifact/').middleware('auth')
+
