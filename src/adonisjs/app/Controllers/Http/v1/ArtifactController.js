2708705d87814afdb510a025e29feabde01d0a63
faguim <pantoja.ti@gmail.com>
2019-05-11 19:07:27 -0300

    Fixing the return of Artifact.store() when caseId doesnt exist

--- src/adonisjs/app/Controllers/Http/v1/ArtifactController.js
+++ src/adonisjs/app/Controllers/Http/v1/ArtifactController.js
@@ -24,48 +24,57 @@ class ArtifactController {
 
 	async store({ request, auth, response }) {
 
+		try{
 
-		const file             = request.file('file', this.validationOptions)
-		const caseID           = request.input('case_id', null)
+			const file             = request.file('file', this.validationOptions)
+			const caseID           = request.input('case_id', null)
+	
+			const artifactID       = await uuid() 
+			const artifactFileName = artifactID + "." + file.extname
+			const fsPath           = Helpers.publicPath(this.relativePath)
+	
+	
+			await file.move(fsPath, {name: artifactFileName, overwrite: false})
+	
+			  if (!file.moved()) 
+				return file.error()
+			
+	
+	
+			var linkedCase = await Case.find(caseID)
 
-		const artifactID       = await uuid() 
-		const artifactFileName = artifactID + "." + file.extname
-		const fsPath           = Helpers.publicPath(this.relativePath)
+			if (caseID != null && linkedCase == null){
+				return response.json({ message: "User id not found" })
+			} 
+	
+	
+			const artifact = new Artifact()
+			artifact.fs_path       = fsPath + artifactFileName 
+			artifact.relative_path = this.relativePath + artifactFileName
+			artifact.case_id       = linkedCase != null ? linkedCase.id : linkedCase;
+			await auth.user.artifacts().save(artifact)
+	
+			 const base_url = Env.getOrFail('APP_URL')
+	
+	
+	
+			return response.status(200).json({ message:       "Artifact successfully stored",
+											   filename:      artifactFileName,
+											   case:          linkedCase,										   
+											   size_in_bytes: file.size,
+											   type:          file.type,
+											   subtype:       file.subtype,
+											   extension:     file.extname,
+											   status:        file.status,
+											   relative_path: artifact.relative_path,
+											   url:           base_url+artifact.relative_path 
+	
+			 })
+		} catch(e){
+			console.log(e)
+      		return response.status(e.status).json({ message: e.message })
+		}
 
-
-		await file.move(fsPath, {name: artifactFileName, overwrite: false})
-
-	  	if (!file.moved()) 
-	    	return file.error()
-	    
-
-
-    	var linkedCase = await Case.find(caseID)
-
-
-
-		const artifact = new Artifact()
-		artifact.fs_path       = fsPath + artifactFileName 
-		artifact.relative_path = this.relativePath + artifactFileName
-		artifact.case_id       = linkedCase != null ? linkedCase.id : linkedCase;
-	    await auth.user.artifacts().save(artifact)
-
- 		const base_url = Env.getOrFail('APP_URL')
-
-
-
-		return response.status(200).json({ message:       "Artifact successfully stored",
-										   filename:      artifactFileName,
-										   case:          linkedCase,										   
-										   size_in_bytes: file.size,
-										   type:          file.type,
-										   subtype:       file.subtype,
-										   extension:     file.extname,
-										   status:        file.status,
-										   relative_path: artifact.relative_path,
-										   url:           base_url+artifact.relative_path 
-
-		 })
 
 	}
 
